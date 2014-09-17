ff0ffd7717efec109d008f8d07770baf06869120
Alfredo Edye <alfredo.edye@gmail.com>
2014-09-16 23:40:12 -0300

    generator-angular-fullstack
    generator-angular-fullstack

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('startApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/start-dev'
+  },
+
+  seedDB: true
+};
